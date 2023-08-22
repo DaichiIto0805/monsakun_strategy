47e04507d096f153f2443beb9fc259e58a9e04b7
daichiito0805 <daichiito0805@gmail.com>
2023-08-23 01:11:42 +0900

    clustering

--- cluster.py
+++ cluster.py
@@ -46,28 +46,46 @@ def plot_dendrogram(model, **kwargs):
     dendrogram(linkage_matrix, **kwargs)
 # %%
 df=pd.read_csv('strategy_all_cross/strategy_all_cross.csv',index_col=0)
-df = df[df['counts']==60]
+# df = df[df['counts']==60]
+df2 = df.copy()
 df = df.drop('counts',axis=1)
 # print(df)
-df2 = df.copy()
-
-df6 = pd.DataFrame(columns=['cluster1','cluster2','cluster3'])
 
 df = df.replace({'F*_f':0,'F*_s':1,'r_f':2,'r_s':3,'s_f':4,'s_s':5,'n_f':6,'n_s':7})
 
-df_cl = pd.DataFrame(index=df.index,columns=[])
+# df_cl = df2['counts']
 
 # %%
-print(len(df.columns)//5)
-for i in range(len(df.columns)//5):
-    df_t = df.iloc[:,i*5:i*5+5]
-    model = AgglomerativeClustering(n_clusters=3)
-    # model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
-    model = model.fit(df_t.values)
-    # plot_dendrogram(model,truncate_mode='lastp',p=3)
-    df_cl['cluster'+str(i)]=model.labels_
+df_cl = df2['counts']
+df_cl_cr = df2['counts']
+k = 0
+for j in range(len(df.columns)//15):
+    df_t = df.iloc[:,j*15:j*15+15]
+    df_t = df_t.dropna()
+    for i in range(len(df_t.columns)//5):
+        df_tt = df_t.iloc[:,i*5:i*5+5]
+        df_tt2 = df_tt.replace({0:'F*_f',1:'F*_s',2:'r_f',3:'r_s',4:'s_f',5:'s_s',6:'n_f',7:'n_s'})
+        model = AgglomerativeClustering(n_clusters=3)
+        # model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+        model = model.fit(df_tt.values)
+        # plot_dendrogram(model,truncate_mode='lastp',p=3)
+        df_tt['cluster'+str(k)]=model.labels_
+        df_tt2['cluster'+str(k)]=model.labels_
+        df_cl = pd.merge(df_cl,df_tt['cluster'+str(k)],how='left',left_index=True,right_index=True)
+        df_cl_cr = pd.merge(df_cl_cr,df_tt2,how='left',left_index=True,right_index=True)
+        print(k)
+        k = k+1
+
+#%%
+df_cl.to_excel('cluster/cluster_all.xlsx')
+df_cl_cr.to_excel('cluster/cluster_all_strategy.xlsx')
+
 #%%
 # X_distance = gower.gower_matrix(df_t)
-# plot_dendrogram(model,truncate_mode='lastp',p=3)
+model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+# model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+model = model.fit(df_tt.values)
+plot_dendrogram(model,truncate_mode='lastp',p=3)
+#%%
 df_cl.to_csv('cluster/cluster_60.csv')
 df2.to_csv('cluster/strategy_all_cross.csv')
