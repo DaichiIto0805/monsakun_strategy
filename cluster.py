44445c8072b5b727dd8cedd0cef17ebb665026e5
daichiito0805 <daichiito0805@gmail.com>
2023-09-28 23:06:17 +0900

    範囲ごとにデンドログラム作成 正解率も考慮したクラスター分析

--- cluster.py
+++ cluster.py
@@ -77,15 +77,102 @@ for j in range(len(df.columns)//15):
         k = k+1
 
 #%%
+df_cl.to_csv('cluster/cluster_60.csv')
+df2.to_csv('cluster/strategy_all_cross.csv')
+#%%
 df_cl.to_excel('cluster/cluster_all.xlsx')
 df_cl_cr.to_excel('cluster/cluster_all_strategy.xlsx')
 
 #%%
-# X_distance = gower.gower_matrix(df_t)
-model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
-# model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
-model = model.fit(df_tt.values)
-plot_dendrogram(model,truncate_mode='lastp',p=3)
+#デンドログラム作成
+p = 4
+for j in range(len(df.columns)//15):
+    df_t = df.iloc[:,j*15:j*15+15]
+    df_t = df_t.dropna()
+    for i in range(len(df_t.columns)//5):
+        df_tt = df_t.iloc[:,i*5:i*5+5]
+        # X_distance = gower.gower_matrix(df_t)
+        model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+        # model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+        model = model.fit(df_tt.values)
+        plot_dendrogram(model,truncate_mode='lastp',p=p)
+        plt.title(df_tt.columns.values[0]+'with'+str(p)+'cluster')
+        plt.savefig('cluster/'+df_tt.columns.values[0]+'_with_'+str(p)+'_cluster')
+        plt.show(df_tt.columns.values[0]+str(p)+'cluster')
+        plt.clf()
 #%%
-df_cl.to_csv('cluster/cluster_60.csv')
-df2.to_csv('cluster/strategy_all_cross.csv')
+
+df3 = df2.drop('counts',axis=1)
+colnames = df3.columns.values
+# print(df3.iloc[:,0])
+for x in range(len(df3.columns)):
+    df3 = pd.concat([df3,df3.iloc[:,x].str.split('_',expand= True)],axis=1).drop(colnames[x],axis=1)
+    df3.rename(columns={0:colnames[x]+'_0',1:colnames[x]+'_1'},inplace=True)
+print(df3)
+#%%
+print(df_tt.columns.values[0])
+
+
+
+#%%
+#正解率を利用したクラスター作成
+df_corate = pd.read_csv('cluster/strategy_all_cross_with_correct_rate.csv',index_col=0)
+df_corate2 = df_corate.copy()
+df_corate = df_corate.drop('counts',axis=1)
+df_corate = df_corate.replace('(.*)_(.*)',r'\1',regex=True)
+df_corate = df_corate.replace({'F*':0,'s':1,'r':2,'n':3})
+print(df_corate.iloc[:,0:18])
+#%%
+#デンドログラム作成（デンドログラムを見てクラスター数を設定）
+clus3col = ['2_1_1','3_3_1','4_2_1','5_1_1']
+clus2col = ['4_3_1','5_3_1']
+for j in range(len(df_corate.columns)//18):
+    df_ct = df_corate.iloc[:,j*18:j*18+18]
+    df_ct = df_ct.dropna()
+    for i in range(len(df_ct.columns)//6):
+        df_ctt = df_ct.iloc[:,i*6:i*6+6]
+        if any(df_ctt.columns.values[0] == col for col in clus3col):
+            p = 3
+        elif any(df_ctt.columns.values[0] == co for co in clus2col):
+            p = 2
+        else:
+            p = 4
+        # X_distance = gower.gower_matrix(df_t)
+        model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+        # model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+        model = model.fit(df_ctt.values)
+        plot_dendrogram(model,truncate_mode='lastp',p=p)
+        plt.title(df_ctt.columns.values[0]+'with'+str(p)+'cluster')
+        plt.savefig('cluster_with_corate/'+df_ctt.columns.values[0]+'_with_'+str(p)+'_cluster_corate')
+        plt.show(df_ctt.columns.values[0]+str(p)+'cluster')
+        plt.clf()
+#%%
+#クラスター作成
+df_cor_cl = df_corate2['counts']
+df_cor_cl_cr = df_corate2['counts']
+k = 0
+for j in range(len(df_corate.columns)//18):
+    df_ct = df_corate.iloc[:,j*18:j*18+18]
+    df_ct = df_ct.dropna()
+    for i in range(len(df_t.columns)//6):
+        df_ctt = df_ct.iloc[:,i*6:i*6+6]
+        if any(df_ctt.columns.values[0] == col for col in clus3col):
+            p = 3
+        elif any(df_ctt.columns.values[0] == co for co in clus2col):
+            p = 2
+        else:
+            p = 4
+        df_ctt2 = df_ctt.iloc[:,0:5].replace({0:'F*',1:'s',2:'r',3:'n'})
+        df_ctt2.insert(5,'correct_rate'+str(k),df_ctt.iloc[:,5])
+        model = AgglomerativeClustering(n_clusters=p)
+        # model = AgglomerativeClustering(distance_threshold=0,n_clusters=None)
+        model = model.fit(df_ctt.values)
+        # plot_dendrogram(model,truncate_mode='lastp',p=3)
+        df_ctt['cluster'+str(k)+'('+str(p)+')']=model.labels_
+        df_ctt2['cluster'+str(k)+'('+str(p)+')']=model.labels_
+        df_cor_cl = pd.merge(df_cor_cl,df_ctt['cluster'+str(k)+'('+str(p)+')'],how='left',left_index=True,right_index=True)
+        df_cor_cl_cr = pd.merge(df_cor_cl_cr,df_ctt2,how='left',left_index=True,right_index=True)
+        print(k)
+        k = k+1
+df_cor_cl.to_csv('cluster_with_corate/cluster_all_corate.csv')
+df_cor_cl_cr.to_excel('cluster_with_corate/cluster_strategy_corate.xlsx')
